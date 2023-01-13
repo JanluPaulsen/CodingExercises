c030ed265a4db4e3f92ccfff4589bfd8062bad0c
Janlu <jpaulsen1317@gmail.com>
2023-01-13 19:09:10 +0200

    More exercises

--- src/Main.java
+++ src/Main.java
@@ -303,5 +303,59 @@ public class Main {
 //                System.out.println("other");
 //            }
 //        }
-    }
+//    }
+
+
+
+    //Number Of Days In Month
+//    public static void main(String[] args) {
+//        System.out.println(isLeapYear(520));
+//        System.out.println(getDaysInMonth(4,2000));
+//    }
+//
+//    public static boolean isLeapYear (int year) {
+//        if (year < 1 || year > 9999){
+//            return false;
+//        }
+//        if (year % 4 == 0) {
+//            if (year % 100 == 0) {
+//                return year % 400 == 0;
+//            }
+//            return true;
+//        }
+//        return false;
+//    }
+//    public static int getDaysInMonth (int month, int year) {
+//        if (month < 1 || month > 12) {
+//            return -1;
+//        } else if (year < 1 || year > 9999) {
+//            return -1;
+//        }
+//        if (isLeapYear(year)) {
+//            switch (month) {
+//                case 1: case 3: case 5: case 7: case 8: case 10: case 12:{
+//                    return 31;
+//                }
+//                case 2:{
+//                    return 29;
+//                }
+//                case 4: case 6: case 9: case 11: {
+//                    return 30;
+//                }
+//            }
+//        } else if (!isLeapYear(year)) {
+//            switch (month) {
+//                case 1: case 3: case 5: case 7: case 8: case 10: case 12:{
+//                    return 31;
+//                }
+//                case 2:{
+//                    return 28;
+//                }
+//                case 4: case 6: case 9: case 11: {
+//                    return 30;
+//                }
+//            }
+//        }
+//        return -1;
+//    }
 }
